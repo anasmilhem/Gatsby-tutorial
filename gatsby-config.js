8e9228245402ee0706c88a6f3f562664bcfee925
Anas Milhem <anasmilhem1997@gmail.com>
2022-02-06 22:18:13 -0500

    update gatsby site

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,9 +1,11 @@
 module.exports = {
     siteMetadata: {
-      title: `My Gatsby Site`,
+        title: `My Gatsby Site`,
         siteUrl: `https://www.yourdomain.tld`,
     },
     plugins: [
+        "gatsby-plugin-image",
+        "gatsby-plugin-sharp",
 
     ]
 }
